c1bbacf46e1f65274480c6546ca2d37224ca534a
PabloPeralta94 <pablo.peralta94@gmail.com>
2023-09-03 15:03:55 -0300

    fin seccion 1

---
+++ holaMundo/holaMundo/src/test/java/curso/spring/holaMundo/HolaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.spring.holaMundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
